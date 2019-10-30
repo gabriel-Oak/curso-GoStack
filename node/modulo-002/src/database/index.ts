4ef2b7a994a579ff5fd1e359888bcaec7cb5689f
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2019-10-30 09:02:45 -0300

    conexão com mongodb

--- node/modulo-002/src/database/index.ts
+++ node/modulo-002/src/database/index.ts
@@ -1,5 +1,6 @@
 import { Sequelize } from 'sequelize';
 import MODELS, { Model } from '../app/models/';
+import mongoose, { Mongoose } from 'mongoose';
 
 
 const connectOption = require('../config/database');
@@ -7,6 +8,7 @@ const connectOption = require('../config/database');
 class Database {
 
     connection: Sequelize;
+    mongoConnection: Mongoose;
 
     constructor() {
         this.connection = new Sequelize(
@@ -19,12 +21,14 @@ class Database {
                 define: connectOption.define
             }
         );
+        this.mongoConnection = mongoose;
 
         this.init();
         this.testConnection();
+        this.mongo();
     }
 
-    init() {
+    private init() {
         MODELS.forEach((model: Model) => {
             model.load(this.connection);
         });
@@ -37,11 +41,37 @@ class Database {
     private testConnection() {
         this.connection.authenticate()
             .then(() => {
-                console.log('Conectado com sucesso ao banco.');
+                console.log('Conectado com sucesso ao banco postgres.');
             })
             .catch((err: any) => {
                 console.error('Não foi possivel conectar ao banco:', err);
             });
+    }
+
+    async mongo() {
+        this.mongoConnection.connect(
+            'mongodb://gobarber:gobarber1@ds018248.mlab.com:18248/nstr',
+            { useNewUrlParser: true, useFindAndModify: true }
+        );
+
+        this.mongoConnection.connection.on('connected', () => {
+            console.log('MongoDB conectado com sucesso!');
+        });
+
+        this.mongoConnection.connection.on('disconnected', () => {
+            console.log('MongoDB desconectado!');
+        });
+
+        this.mongoConnection.connection.on('error', e => {
+            console.log('MongoDB ERRO: ' + e);
+        });
+
+        process.on('SIGINT', () => {
+            this.mongoConnection.connection.close(() => {
+                console.log('MongoDB desconectado pelo término da aplicação');
+                process.exit(0);
+            });
+        });
     }
 }
 
