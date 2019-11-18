664693757720c97d524e1e835f26f773b71ef976
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2019-11-18 15:10:12 -0300

    template email

--- node/modulo-002/src/app/lib/mailer.ts
+++ node/modulo-002/src/app/lib/mailer.ts
@@ -1,6 +1,18 @@
 import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
 import Mail from 'nodemailer/lib/mailer';
 import configs from '../../config/mails';
+
+const nodemailerhbs = require('nodemailer-express-handlebars');
+
+interface Message {
+    to: string;
+    subject: string;
+    text?: string;
+    template?: string;
+    context?: any;
+}
 
 class Mailer {
 
@@ -8,9 +20,24 @@ class Mailer {
 
     constructor() {
         this.transporter = nodemailer.createTransport(configs);
+        this.configureTemplates();
     }
 
-    public sendMail(message: any) {
+    private configureTemplates() {
+        const viewPath = resolve(__dirname, '..', 'views', 'emails');
+        this.transporter.use('compile', nodemailerhbs({
+            viewEngine: exphbs.create({
+                layoutsDir: resolve(viewPath, 'layouts'),
+                partialsDir: resolve(viewPath, 'partials'),
+                defaultLayout: 'default',
+                extname: '.hbs'
+            }),
+            viewPath,
+            extname: '.hbs'
+        }));
+    }
+
+    public sendMail(message: Message) {
         return this.transporter.sendMail({
             ...configs.default,
             ...message
