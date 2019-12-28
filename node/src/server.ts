65c17ed005f08f7e81d2a4c95073a065b82c4d82
gabriel-Oak <gabriel.costa41@fatec.sp.gov.br>
2019-12-28 09:06:52 -0300

    --

--- node/src/server.ts
+++ node/src/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const port = 3000
 
 app.listen(port || process.env.port, () => {
-    console.log(`Rodando na ${port}`);
+    console.log(`Rodando na ${port || process.env.port}`);
 });
